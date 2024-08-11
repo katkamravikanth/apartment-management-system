84a38f88528a102f0dc716fd7dd79f4708ee0f91
Ravikanth Katkam <katkamravikanth22@gmail.com>
2024-08-11 13:34:44 +0530

    admin page and admin user page added

---
+++ web/src/Controller/AdminUserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
+
+class AdminUserController extends AbstractController
+{
+    #[Route('/admin/users', name: 'admin_users')]
+    public function index(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAll();
+
+        return $this->render('admin/users.html.twig', [
+            'users' => $users,
+        ]);
+    }
+}
